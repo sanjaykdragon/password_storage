d524aae16c683eea29899089619bef2025a376e7
Sanjay K <sanjaykdragon@gmail.com>
2019-11-05 18:11:10 -0500

    added
    added

---
+++ password_storage/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace password_storage
+{
+    class Program
+    {
+
+        //https://stackoverflow.com/questions/2883576/how-do-you-convert-epoch-time-in-c
+        public static DateTime FromUnixTime(long unix_time)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unix_time);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "password manager @sanjaykdragon";
+            Console.WriteLine("password manager by sanjaykdragon");
+            Console.WriteLine("to do: automatic DB saves, local config files, encryption");
+            Console.WriteLine("enter 1) get db, 2) save credentials");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                char first_char = input.Length > 0 ? input.ToLower().ToCharArray()[0] : ' ';
+
+                if (first_char == '1' || first_char == 'g')
+                {
+                    //get db
+                    var response = network.get_database();
+                    if (response.status == "failed")
+                    {
+                        Console.WriteLine("error occurred: {0}", response.detail);
+                    }
+                    else
+                    {
+                        foreach (var item in response.values)
+                        {
+                            string decrypted_site = item.site;
+                            string decrypted_username = item.username;
+                            string decrypted_password = item.password;
+                            Console.WriteLine("site: {0}, username: {1}, password: {2}", decrypted_site, decrypted_username, decrypted_password);
+                        }
+                    }
+                }
+                else if (first_char == '2' || first_char == 's')
+                {
+                    //save creds
+                    Console.WriteLine("enter the name of the site: ");
+                    string site = Console.ReadLine();
+                    Console.WriteLine("enter the username for this site: ");
+                    string username = Console.ReadLine();
+                    Console.WriteLine("enter the password for this site: ");
+                    string password = Console.ReadLine();
+
+                    var response = network.send_credentials(username, password, site);
+                    if (response.status == "failed")
+                    {
+                        Console.WriteLine("error occurred: {0}", response.detail);
+                    }
+                    else
+                    {
+                        Console.WriteLine("added to db successfully");
+                    }
+                }
+                Console.WriteLine("enter 1) get db, 2) save credentials");
+            }
+        }
+    }
+}
---
+++ web_files/setup.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 06, 2019 at 12:07 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pw_mgr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `info`
+--
+
+CREATE TABLE `info` (
+  `site` text NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
